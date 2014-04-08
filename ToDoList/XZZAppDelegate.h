9f44169391487eec853e493d6740a727f54f0c5d
Zac <zac@192-168-1-9.tpgi.com.au>
2014-04-08 17:13:22 +1000

    Initial Commit

---
+++ ToDoList/XZZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XZZAppDelegate.h
+//  ToDoList
+//
+//  Created by Zac on 8/04/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XZZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoListTests/ToDoListTests.m
@@ -0,0 +1,34 @@
+//
+//  ToDoListTests.m
+//  ToDoListTests
+//
+//  Created by Zac on 8/04/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ToDoListTests : XCTestCase
+
+@end
+
+@implementation ToDoListTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
